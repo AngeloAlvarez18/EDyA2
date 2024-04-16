451ccdb135bab37f5a1937428d3b95845dbee490
AngeloAlvarez18 <alvarezangelo92@gmail.com>
2024-04-16 16:01:26 -0300

    terminando lab1

--- U2/lab1.hs
+++ U2/lab1.hs
@@ -117,9 +117,9 @@ de cuatro. (Diccionario de la Real Academia Espaola, 22ª ed.)
 
 ¿Cuál es el tipo de la función definida?
 -}
-bisiesto :: Int -> Bool
-bisiesto x | vabs(2024 - x) % 4 == 0 = True
-
+-- bisiesto :: Int -> Bool
+-- bisiesto x | vabs(2024 - x) % 4 == 0 = True
+--            | otherwise = False
 {-
 4)
 
@@ -136,28 +136,53 @@ expresión sea válida:
 
 -}
 
--- v = [1, 2, 3] *$ 2 *$ 4
+
+(*$) :: [Int] -> Int -> [Int]
+[] *$ ys = []
+(x:xs) *$ ys = (x * ys) : xs *$ ys
+
+v = [1, 2, 3] *$ 2 *$ 4
 
 
-{-
-5) Definir las siguientes funciones usando listas por comprensión:
 
-a) 'divisors', que dado un entero positivo 'x' devuelve la
-lista de los divisores de 'x' (o la lista vacía si el entero no es positivo)
+-- 5) Definir las siguientes funciones usando listas por comprensión:
 
-b) 'matches', que dados un entero 'x' y una lista de enteros descarta
-de la lista los elementos distintos a 'x'
+-- a) 'divisors', que dado un entero positivo 'x' devuelve la
+-- lista de los divisores de 'x' (o la lista vacía si el entero no es positivo)
 
-c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
-'(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
-donde 0 <= a, b, c, d <= 'n'
+divisors :: Int -> [Int]
+divisors n = [x | x <- [1..n], n `mod` x == 0]
 
-(d) 'unique', que dada una lista 'xs' de enteros, devuelve la lista
-'xs' sin elementos repetidos
--}
+-- b) 'matches', que dados un entero 'x' y una lista de enteros descarta
+-- de la lista los elementos distintos a 'x'
 
+matches :: Int -> [Int] -> [Int]
+matches a b = [x | x <- b, x == a]
+
+-- c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
+-- '(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
+-- donde 0 <= a, b, c, d <= 'n'
+
+cuadrupla :: Int -> [(Int, Int, Int, Int)]
+cuadrupla n = [(a,b,c,d) | a <- [0..n], b <- [0..n], c <- [0..n], d <- [0..n], a^2 + b^2 == c^2 + d^2]
+
+-- (d) 'unique', que dada una lista 'xs' de enteros, devuelve la lista
+-- 'xs' sin elementos repetidos
 -- unique :: [Int] -> [Int]
--- unique xs = [x | (x,i) <- zip xs [0..], not (elem x (take i xs))]
+unique xs = [x | (x,i) <- zip xs [0..], not (elem x (take i xs))]
+
+-- como funciona:
+
+-- 1- zip xs [0..]: Esta expresión crea una lista de tuplas donde cada elemento de xs se combina con su índice correspondiente. Por ejemplo, si xs = [3,1,2], entonces zip xs [0..] produce [(3,0),(1,1),(2,2)].
+
+-- 2- (x,i) <- zip xs [0..]: Utiliza una lista por comprensión para recorrer la lista de tuplas generada por zip. En cada iteración, x es el elemento de xs y i es su índice correspondiente.
+
+-- 3- take i xs: Toma los primeros i elementos de xs. Esto significa que en cada iteración estamos tomando los elementos anteriores a x en la lista original.
+
+-- 4- not (elem x (take i xs)): Verifica si x no está presente en los elementos anteriores a él en la lista original. En otras palabras, estamos verificando si x es único hasta ese punto en la lista original.
+
+-- 5- [x | (x,i) <- zip xs [0..], not (elem x (take i xs))]: Esta es la parte principal de la función. Estamos utilizando una lista por comprensión para iterar sobre las tuplas generadas por zip. En cada iteración, si x es único hasta ese punto en la lista original, se agrega a la lista resultante.
+
 
 {- 
 6) El producto escalar de dos listas de enteros de igual longitud
@@ -167,43 +192,85 @@ devuelva el producto escalar de dos listas.
 
 Sugerencia: Usar las funciones 'zip' y 'sum'. -}
 
-{-
-7) Definir mediante recursión explícita
-las siguientes funciones y escribir su tipo más general:
+tupleProd :: [(Int, Int)] -> [Int]
+tupleProd [] = []
+tupleProd ((a,b):xs) = a * b : tupleProd xs
 
-a) 'suma', que suma todos los elementos de una lista de números
+summList :: [Int] -> Int
+summList [] = 0
+summList (x:xs) = x + summList xs
 
-b) 'alguno', que devuelve True si algún elemento de una
-lista de valores booleanos es True, y False en caso
-contrario
+scalarProduct :: [Int] -> [Int] -> Int
+scalarProduct x y | not (lengthh x == lengthh y) = error "diferent lenght"
+                  | otherwise = summList (tupleProd (zip x y))
+            
 
-c) 'todos', que devuelve True si todos los elementos de
-una lista de valores booleanos son True, y False en caso
-contrario
+-- 7) Definir mediante recursión explícita
+-- las siguientes funciones y escribir su tipo más general:
 
-d) 'codes', que dada una lista de caracteres, devuelve la
-lista de sus ordinales
+-- a) 'suma', que suma todos los elementos de una lista de números
 
-e) 'restos', que calcula la lista de los restos de la
-división de los elementos de una lista de números dada por otro
-número dado
+-- Ya hecha en summList
 
-f) 'cuadrados', que dada una lista de números, devuelva la
-lista de sus cuadrados
+-- b) 'alguno', que devuelve True si algún elemento de una
+-- lista de valores booleanos es True, y False en caso
+-- contrario
 
-g) 'longitudes', que dada una lista de listas, devuelve la
-lista de sus longitudes
+alguno :: [Bool] -> Bool
+alguno [] = False
+alguno (x:xs) = x || alguno xs
 
-h) 'orden', que dada una lista de pares de números, devuelve
-la lista de aquellos pares en los que la primera componente es
-menor que el triple de la segunda
+-- c) 'todos', que devuelve True si todos los elementos de
+-- una lista de valores booleanos son True, y False en caso
+-- contrario
 
-i) 'pares', que dada una lista de enteros, devuelve la lista
-de los elementos pares
+todos :: [Bool] -> Bool
+todos [] = True
+todos (x:xs) = x && todos xs
 
-j) 'letras', que dada una lista de caracteres, devuelve la
-lista de aquellos que son letras (minúsculas o mayúsculas)
+-- d) 'codes', que dada una lista de caracteres, devuelve la
+-- lista de sus ordinales
 
-k) 'masDe', que dada una lista de listas 'xss' y un
-número 'n', devuelve la lista de aquellas listas de 'xss'
-con longitud mayor que 'n' -}
+codes :: [Char] -> [Int]
+codes [] = []
+codes (x:xs) = fromEnum x : codes xs
+
+-- e) 'restos', que calcula la lista de los restos de la
+-- división de los elementos de una lista de números dada por otro
+-- número dado
+
+restos:: [Int] -> Int -> [Int]
+restos [] y = []
+restos (x:xs) y = x `mod` y : restos xs y
+
+-- f) 'cuadrados', que dada una lista de números, devuelva la
+-- lista de sus cuadrados
+
+cuadrados :: [Int] -> [Int]
+cuadrados [] = []
+cuadrados (x:xs) = x * x : cuadrados xs
+
+-- g) 'longitudes', que dada una lista de listas, devuelve la
+-- lista de sus longitudes
+
+longitudes :: [[a]] -> [Int]
+longitudes [] = []
+longitudes (x:xs) = lengthh x : longitudes xs
+
+-- h) 'orden', que dada una lista de pares de números, devuelve
+-- la lista de aquellos pares en los que la primera componente es
+-- menor que el triple de la segunda
+
+orden :: [(Int,Int)] -> [(Int, Int)]
+orden [] = []
+orden ((a,b):xs) = if a < (b * 3) then (a, b) : orden xs else orden xs
+
+-- i) 'pares', que dada una lista de enteros, devuelve la lista
+-- de los elementos pares
+
+-- j) 'letras', que dada una lista de caracteres, devuelve la
+-- lista de aquellos que son letras (minúsculas o mayúsculas)
+
+-- k) 'masDe', que dada una lista de listas 'xss' y un
+-- número 'n', devuelve la lista de aquellas listas de 'xss'
+-- con longitud mayor que 'n' -}
---
+++ U2/practica2.hs
@@ -0,0 +1,10 @@
+module Lab01 where
+
+import Data.List
+import Prelude.hs
+
+1. Dar el tipo completo de las siguientes funciones:
+a) test donde test f x = f x ≡ x + 2
+b) esMenor donde esMenor y z = y < z
+c) eq donde eq a b = a ≡ b
+d) showVal donde showVal x = "Valor:" ++ show x
