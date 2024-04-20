1218090500f71c15b227e9fa8ebfbd770ed7a6e6
AngeloAlvarez18 <alvarezangelo92@gmail.com>
2024-04-20 16:42:01 -0300

    lab1compl

---
+++ U2/lab1compl.hs
@@ -0,0 +1,93 @@
+module Lab1C where
+
+import Data.List
+import Data.Ord
+
+type Texto = String
+
+{-
+   Definir una función que dado un caracter y un texto calcule la frecuencia 
+   con la que ocurre el caracter en el texto
+   Por ejemplo: frecuency 'a' "casa" = 0.5 
+-}
+
+lengthh :: [a] -> Float
+lengthh []        =  0
+lengthh (_:l)     =  1 + lengthh l
+
+--frecuency :: Char -> Texto -> Float
+frecuency c s = (lengthh x) / (lengthh s) where x = [a | a <- s, a == c]
+
+{-                      
+  Definir una función frecuencyMap que dado un texto calcule la frecuencia 
+  con la que ocurre cada caracter del texto en éste.
+  La lista resultado debe estar ordenada respecto a la frecuencia con la que ocurre 
+  cada caracter, de menor a mayor frecuencia. 
+    
+  Por ejemplo: frecuencyMap "casa" = [('c',0.25),('s',0.25),('a',0.5)]
+
+-}
+
+merge ([], ys) = ys
+merge (xs, []) = xs
+merge ((a,b):xs, (c,d):ys) = if b <= d then (a,b):merge(xs, (c,d):ys) else (c,d):merge((a,b):xs,ys)
+
+split [] = ([], [])
+split [x] = ([x], [])
+split (x:y:zs) = let (xs,ys) = split zs in (x:xs, y:ys)
+
+mergeSortTuple [] = []
+mergeSortTuple [x] = [x]
+mergeSortTuple xs = let (ls, rs) = split xs
+                        ls' = mergeSortTuple ls
+                        rs' = mergeSortTuple rs
+                    in merge (ls', rs')
+
+
+unique xs = [x | (x,i) <- zip xs [0..], not (elem x (take i xs))]
+
+frecuencyMap :: Texto -> [(Char, Float)]
+frecuencyMap s =  mergeSortTuple (unique ([(a,frecuency a s) | a <- s]))
+
+{-
+  Definir una función subconjuntos, que dada una lista xs devuelva una lista 
+  con las listas que pueden generarse con los elementos de xs.
+
+  Por ejemplo: subconjuntos [2,3,4] = [[2,3,4],[2,3],[2,4],[2],[3,4],[3],[4],[]]
+-}
+
+subconjuntos :: [a] -> [[a]]
+subconjuntos [] = [[]]
+subconjuntos (x:xs) = map (x:) (subconjuntos xs) ++ subconjuntos xs
+
+-- map 2: (map 3: subconjuntos [4])
+-- map 2: (map 3: (map 4: subconjuntos []))
+-- map 2: (map 3: (map 4: [[]]))
+-- map 2: (map 3: [[4]])
+-- map 2: [[3,4]]
+-- [[2,3,4]]
+
+
+{-
+ Definir una función intercala :: a -> [a] -> [[a]]
+ tal que (intercala x ys) contiene las listas que se obtienen
+ intercalando x entre los elementos de ys. 
+ 
+ Por ejemplo: intercala 1 [2,3]  ==  [[1,2,3],[2,1,3],[2,3,1]]
+-}
+
+
+intercala :: a -> [a] -> [[a]]
+intercala x ys = 
+
+1 [2,3] = 
+
+{- 
+  Definir una función permutaciones que dada una lista calcule todas las permutaciones
+  posibles de sus elementos. Ayuda: la función anterior puede ser útil. 
+
+  Por ejemplo: permutaciones "abc" = ["abc","bac","cba","bca","cab","acb"]
+-}                  
+
+permutaciones :: [a] -> [[a]]
+permutaciones = undefined 
--- U2/practica2.hs
+++ U2/practica2.hs
@@ -165,8 +165,202 @@ identidad2 x = x
 -- 5. Reescribir cada una de las siguientes definiciones sin usar let, where o if :
 -- a) f x = let (y, z ) = (x , x ) in y
 
+-- f x = let (y, z) = (x , x) -- Declara la tupla (y,z) que es igual a (x, x)
+--               in y  -- Devuelve el primer argumento de la tupla creada, o sea y = x
 
+f x = x
 
 -- b) greater (x , y) = if x > y then True else False
 
+greater :: (Int, Int) -> Bool
+greater (x, y) = x > y
+
 -- c) f (x , y) = let z = x + y in g (z , y) where g (a, b) = a − b
+
+f2 (x , y) = x
+
+
+-- 6. Pasar de notacion Haskell a notacion de funciones anonimas (llamada notacion lambda),
+-- a) smallest, definida por
+-- smallest (x , y, z ) | x <= y && x <= z = x
+--                      | y <= x && y <= z = y
+--                      | z <= x && z <= y = z
+
+smallest :: Int -> Int -> Int -> Int
+smallest = \x y z -> if ((x <= y) && (x <= z)) then x else if ((y <= x) && (y <= z)) then y else z
+
+
+-- b) second x = λx → x
+
+second :: Int -> Int
+second = \x -> x
+
+-- c) andThen, definida por
+-- andThen True y = y
+-- andThen False y = False
+
+--andThen :: Bool -> a -> a
+andThen = \x y -> if x then y else x -- No se porque no FUNCA
+
+-- d) twice f x = f (f x )
+
+twice :: (Int -> Int) -> Int -> Int
+twice = \f x -> f (f x)
+
+-- e) flip f x y = f y x
+
+flip = \f x y -> f y x
+
+--f) inc = (+1)
+
+inc = \x -> x + 1
+
+
+-- 7. Pasar de notacion lambda a notacion Haskell,
+-- a) iff = λx → λy → if x then not y else y
+iff :: Bool -> Bool -> Bool
+iff x y = if x then not y else y
+
+
+-- 8. Suponiendo que f y g tienen los siguientes tipos
+-- f :: c → d
+-- g :: a → b → c
+-- y sea h definida como
+-- h x y = f (g x y)
+-- Determinar el tipo de h e indicar cuales de las siguientes definiciones de h son equivalentes a la dada:
+-- h = f ◦ g
+-- h x = f ◦ (g x )
+-- h x y = (f ◦ g) x y    (Esta es la correcta)
+-- Dar el tipo de la funcion (◦)
+
+-- h :: a -> b -> d
+
+-- (.) :: (b -> c) -> (a- > b) -> (a -> c)
+
+-- 9. La funcion zipp3 zipea 3 listas. Dar una definicion recursiva de la funcion y 
+-- otra definicion con el mismo tipo que utilice la funcion zip. 
+-- ¿Que ventajas y desventajas tiene cada definicion?
+
+-- Aca entendi mal e hice una funcion q concatena 3 listas xD
+
+-- zipp3 :: [a] -> [a] -> [a] -> [a]
+-- zipp3 [] [] z = z
+-- zipp3 [] (y:ys) z = y : zipp3 [] ys z
+-- zipp3 (x:xs) y z = x : zipp3 xs y z
+
+-- zipp3 :: [a] -> [b] -> [c] -> [(a,b,c)]
+-- zipp3 
+
+zip_hecha :: [a] -> [b] -> [(a,b)]
+zip_hecha [] y = []
+zip_hecha x [] = []
+zip_hecha (x:xs) (y:ys) = (x, y) : zip_hecha xs ys
+
+zipp3 :: [a] -> [b] -> [c] -> [(a,b,c)]
+zipp3 [] [] z = []
+zipp3 x [] [] = []
+zipp3 [] y [] = []
+zipp3 [] y z = []
+zipp3 x [] z = []
+zipp3 x y [] = []
+zipp3 (x:xs) (y:ys) (z:zs) = (x,y,z) : zipp3 xs ys zs
+
+zipp3_ :: [a] -> [b] -> [c] -> [(a,b,c)]
+zipp3_ x y z = f x d 
+    where 
+        d = zip y z
+        f [] b = []
+        f a [] = []
+        f (a:as) ((o,p):bs) = (a,o,p) : f as bs
+
+-- La ventajas de usar zip es que tenemos menos casos base para la definicion de la funcion
+
+-- 10. Indicar bajo que suposiciones tienen sentido las siguientes ecuaciones. Para aquellas que tengan sentido, indicar
+-- si son verdaderas y en caso de no serlo modificar su lado derecho para que resulten verdaderas:
+-- a) [[]] ++ xs = xs
+-- Tiene sentido solo si xs = [[a]] o xs = [], [[]] ++ xs = [] : xs
+
+
+-- b) [[]] ++ xs = [xs]
+-- Tiene sentido solo si xs = [[a]] o xs = [], [[]] ++ xs = [] : xs
+
+-- c) [[]] ++ xs = [] : xs
+-- Tiene sentido solo si xs = [[a]] o xs = [], el resultado es verdadero
+
+-- d) [[]] ++ xs = [[], xs]
+-- Tiene sentido solo si xs = [[a]] o xs = [], [[]] ++ xs = [] : xs
+
+-- e) [[]] ++ [xs] = [[], xs]
+-- Tiene sentido, el resultado esta bien
+
+-- f) [[]] ++ [xs] = [xs]
+-- Tiene sentido, el resultado es [[], xs]
+
+-- g) [] ++ xs = [] : xs
+-- Tiene sentido solo si xs = [] o xs = [[a]], el resultado es [xs]
+
+-- h) [] ++ xs = xs
+-- Tiene sentido solo si xs = [] o xs = [[a]], el resultado ta bien
+
+-- i) [xs] ++ [] = [xs]
+-- Tiene sentido solo si xs = [] o xs = [[a]], el resultado ta bien
+
+-- j) [xs] ++ [xs] = [xs, xs]
+-- Tiene sentido solo si xs = [] o xs = [[a]], el resultado ta bien
+
+
+-- 11. Inferir, de ser posible, los tipos de las siguientes funciones:
+-- (puede suponer que sqrt :: Float → Float)
+
+
+-- a) modulus = sqrt ◦ sum ◦ map (↑2)
+modulus = sqrt . sum . map (^2)
+
+-- b) vmod [ ] = [ ]
+-- vmod (v : vs) = modulus v : vmod vs
+
+-- ni idea
+
+-- 12. Dado el siguiente tipo para representar numeros binarios:
+-- type NumBin = [Bool]
+-- donde el valor False representa el numero 0 y True el 1. Definir las siguientes operaciones tomando como convencion
+-- una representacion Little-Endian (i.e. el primer elemento de las lista de dıgitos es el dıgito menos significativo del
+-- numero representado).
+-- a) suma binaria
+
+-- type NumBin = [Bool]
+-- bin_sum :: NumBin -> NumBin -> NumBin
+-- bin_sum x y = if lengthh x >= y then f x y else 
+
+
+-- (True, False, True) + (False, True)
+
+
+-- b) producto binario
+-- c) cociente y resto de la division por dos
+
+
+-- 13. Definir las siguientes funciones usando listas por comprension:
+-- a) divisors, que dado un entero positivo x devuelve la lista de los divisores de x (y la lista vacıa si el entero no es
+-- positivo).
+
+divisors n = [x | x <- [1..n], n `mod` x == 0]
+
+-- b) matches, que dados un entero x y una lista de enteros descarta de la lista los elementos distintos a x .
+
+matches n m = [x | x <- m, x == n]
+
+-- c) cuadruplas, que dado un natural n, devuelve las cuadruplas (a, b, c, d ) con 0 < a, b, c, d , 6 n que cumplen
+-- a ↑ 2 + b ↑ 2 = c ↑ 2 + d ↑ 2.
+
+cuadruplas n = [(a,b,c,d) | a <- [0..n], b <- [0..n], c <- [0..n], d <- [0..n], a*a + b*b == c*c + d*d]
+
+
+-- d) unique, que dada una lista xs de enteros, devuelve la lista con los elementos no repetidos de xs.
+-- Por ejemplo, unique [1, 4, 2, 1, 3] = [1, 4, 2, 3].
+
+-- 14. El producto escalar de dos listas de enteros de igual longitud es la suma de los productos de los elementos
+-- sucesivos (misma posicion) de ambas listas. Usando listas por comprension defina una funcion scalarproduct que
+-- devuelva el producto escalar de dos listas.
+
+-- hecho en lab1
