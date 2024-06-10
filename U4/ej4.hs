7a79d5e044960271ad986746a35623926ed5210c
AngeloAlvarez18 <alvarezangelo92@gmail.com>
2024-06-10 19:27:16 -0300

    ej4

--- U4/ej4.hs
+++ U4/ej4.hs
@@ -8,14 +8,39 @@ altura (N l x r ) = 1 + max (altura l) (altura r)
 
 -- a)
 
-combinar :: T a -> T a -> T a
-combinar E t = t
-combinar t E = t
-combinar (N l a r) (N (N lr b' rr) b E) = N (combinar l (N lr b' E)) a (combinar r (N rr b E))
-combinar (N l a r) (N l' b r') = N (combinar l (N l' b E)) a (combinar r r')
+-- combinar :: T a -> T a -> T a
+-- combinar E t = t
+-- combinar t E = t
+-- combinar (N l a r) (N (N lr b' rr) b E) = N (combinar l (N lr b' E)) a (combinar r (N rr b E))
+-- combinar (N l a r) (N l' b r') = N (combinar l (N l' b E)) a (combinar r r')
 
 
 a = N (N (N E 4 E) 2 (N E 5 E)) 1 (N (N E 6 E) 3 (N E 7 E))
 b = N (N (N E 11 E) 9 (N E 12 E)) 8 (N (N E 13 E) 10 (N E 14 E))
 
-N (N (N (N E 11 E) 4 (N E 9 E)) 2 (N (N E 12 E) 5 (N E 8 E))) 1 (N (N (N E 13 E) 6 (N E 10 E)) 3 (N (N E 14 E) 7 E))
+--tomarMenor :: T Int -> (Int, T Int)
+tomarMenor (N E a r) = (a, r)
+tomarMenor (N (N E b rr) a r) = (b, N rr a r)
+tomarMenor (N l a r) = let (x, tl) = tomarMenor l
+                        in (x, (N tl a r))
+
+--combinar :: T Int -> T Int -> T Int
+combinar E t = t
+combinar t1@(N l a r) t = let (x, t') = tomarMenor t1
+                        in (N t' x t)
+
+
+
+--filterT :: (Int -> Bool) -> T Int -> T Int
+filterT f E = E
+filterT f (N l a r) | (f a) = N (filterT f l) a (filterT f r)
+                   | otherwise = combinar (filterT f l) (filterT f r)
+
+--par :: Int -> Bool
+par n = (mod n 2) == 0
+
+quickSort :: T Int -> T Int
+quickSort E = E
+quickSort t@(N l a r) = let lr = filterT (<a) t 
+                            rr = filterT (>a) t
+                    in N (quickSort lr) a (quickSort rr)
